ee5cb1c3a572331ac1d4ea61a879eb4b2b05aa5d
holczur <holcz.norbert@gmail.com>
2021-02-16 08:02:45 +0100

    navbar issues have been fixed

--- bookmark-landing-page/script.js
+++ bookmark-landing-page/script.js
@@ -12,22 +12,24 @@ main.addEventListener('click', () => {
 })
 */
 
+
 const closeBtn = document.getElementById('close')
 const openBtn = document.getElementById('open')
 const navMenu = document.querySelector('.nav-menu')
 const closedNav = document.querySelector('.closed-nav')
+const navMenuLogo = document.getElementById('nav-menu-logo')
+const navSocialMedia = document.querySelector('.social-media')
+const navList = document.querySelector('.nav-list')
 
 openBtn.addEventListener('click', () => {
-    navMenu.classList.remove('hidden-nav');
+    navMenu.classList.remove('hidden-nav')
+    closeBtn.classList.remove('hide');
     closedNav.classList.add('hidden-nav');
-    closeBtn.classList.remove('hide') 
 })
 
 closeBtn.addEventListener('click', () => {
-    navMenu.classList.add('hidden-nav');
+    navMenu.classList.add('hidden-nav')
+    closeBtn.classList.add('hide');
     closedNav.classList.remove('hidden-nav');
-    closeBtn.classList.add('hide')       
 })
-
-
 
