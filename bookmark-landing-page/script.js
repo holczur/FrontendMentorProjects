959cb63f0f23af5c258f68745d155b567a89613b
holczur <holcz.norbert@gmail.com>
2021-02-18 13:03:22 +0100

    Features section is ready

--- bookmark-landing-page/script.js
+++ bookmark-landing-page/script.js
@@ -3,7 +3,14 @@ const openNav = document.getElementById('open-nav')
 const closeBtn = document.getElementById('close')
 const closedNav = document.querySelector('.closed-nav')
 
+const featureLinks = document.querySelectorAll('.feature-link')
+const simple = document.getElementById('simple')
+const easy = document.getElementById('easy')
+const share = document.getElementById('share')
+const featureImg = document.getElementById('changing-img')
+const featureDescriptions = document.querySelectorAll('.feature')
 
+/* NAV MENU **************************************************/
 openBtn.addEventListener('click', () => {
     closedNav.classList.replace('flex-row-between', 'unvisible');
     openNav.classList.replace('unvisible' , 'flex-col-center');
@@ -12,8 +19,25 @@ openBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     closedNav.classList.replace('unvisible' , 'flex-row-between');
-    openNav.classList.replace('flex-col-center' , 'unvisible');
+    openNav.classList.add('unvisible');
+    setTimeout(function(){ openNav.classList.remove('flex-col-center'); }, 500);
     closeBtn.classList.add('hide');
 })
 
+/* FEATURES ****************************************************/
 
+
+for (let i = 0; i < featureLinks.length; i++){
+    featureLinks[i].addEventListener('click', () => {
+        featureLinks.forEach(link => {
+            link.classList.remove('active')
+        });
+        featureImg.src = `./img/illustration-features-tab-${i + 1}.svg`;
+        featureLinks[i].classList.add('active');
+        featureDescriptions.forEach(description => {
+            description.classList.add('hide');
+        });
+        featureDescriptions[i].classList.remove('hide');
+    })
+
+}
