2ada35d3f965ffaad3d62ad1b167bc6f8a5ab9f9
holczur <holcz.norbert@gmail.com>
2021-02-18 08:54:59 +0100

    Header section is ready

--- bookmark-landing-page/script.js
+++ bookmark-landing-page/script.js
@@ -1,35 +1,19 @@
-/*
-const menu = document.getElementById('menu')
-const navLinks = document.getElementById('nav-links')
-const main = document.querySelector('main')
-
-menu.addEventListener('click', () => {
-    navLinks.classList.toggle('hide');
-})
-
-main.addEventListener('click', () => {
-    navLinks.classList.add('hide');
-})
-*/
-
-
-const closeBtn = document.getElementById('close')
 const openBtn = document.getElementById('open')
-const navMenu = document.querySelector('.nav-menu')
+const openNav = document.getElementById('open-nav')
+const closeBtn = document.getElementById('close')
 const closedNav = document.querySelector('.closed-nav')
-const navMenuLogo = document.getElementById('nav-menu-logo')
-const navSocialMedia = document.querySelector('.social-media')
-const navList = document.querySelector('.nav-list')
+
 
 openBtn.addEventListener('click', () => {
-    navMenu.classList.remove('hidden-nav')
+    closedNav.classList.replace('flex-row-between', 'unvisible');
+    openNav.classList.replace('unvisible' , 'flex-col-center');
     closeBtn.classList.remove('hide');
-    closedNav.classList.add('hidden-nav');
 })
 
 closeBtn.addEventListener('click', () => {
-    navMenu.classList.add('hidden-nav')
+    closedNav.classList.replace('unvisible' , 'flex-row-between');
+    openNav.classList.replace('flex-col-center' , 'unvisible');
     closeBtn.classList.add('hide');
-    closedNav.classList.remove('hidden-nav');
 })
+
 
