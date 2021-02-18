b6462917f96ea0e3e72265d5f49c022f132a9f29
holczur <holcz.norbert@gmail.com>
2021-02-18 16:05:27 +0100

    faq section is ready

--- bookmark-landing-page/script.js
+++ bookmark-landing-page/script.js
@@ -10,8 +10,12 @@ const share = document.getElementById('share')
 const featureImg = document.getElementById('changing-img')
 const featureDescriptions = document.querySelectorAll('.feature')
 
+const faqs = document.querySelectorAll('.faq-holder')
+const chevrons = document.querySelectorAll('.faq-holder .fas')
+
 /* NAV MENU **************************************************/
 openBtn.addEventListener('click', () => {
+    openNav.classList.remove('hide')
     closedNav.classList.replace('flex-row-between', 'unvisible');
     openNav.classList.replace('unvisible' , 'flex-col-center');
     closeBtn.classList.remove('hide');
@@ -20,7 +24,10 @@ openBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     closedNav.classList.replace('unvisible' , 'flex-row-between');
     openNav.classList.add('unvisible');
-    setTimeout(function(){ openNav.classList.remove('flex-col-center'); }, 500);
+    setTimeout(function(){
+        openNav.classList.remove('flex-col-center');
+        openNav.classList.add('hide');
+    }, 500);
     closeBtn.classList.add('hide');
 })
 
@@ -40,4 +47,19 @@ for (let i = 0; i < featureLinks.length; i++){
         featureDescriptions[i].classList.remove('hide');
     })
 
+}
+
+/* FAQ *******************************************************************/
+
+for (let i = 0; i < faqs.length; i++) {
+    faqs[i].addEventListener('click', () => {
+        faqs[i].classList.toggle('active');
+        if (faqs[i].classList.contains('active')) {
+            chevrons[i].classList.remove('fa-chevron-down');
+            chevrons[i].classList.add('fa-chevron-up');
+        } else {
+            chevrons[i].classList.add('fa-chevron-down');
+            chevrons[i].classList.remove('fa-chevron-up');
+        }
+    })
 }
