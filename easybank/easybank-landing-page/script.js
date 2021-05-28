976885560282260b9d2b9ae38a2f4a2e43cbecc4
holczur <holcz.norbert@gmail.com>
2021-05-28 14:00:14 +0200

    navigation menu is ready

--- easybank/easybank-landing-page/script.js
+++ easybank/easybank-landing-page/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const menuBtn = document.querySelector('.menu-btn');
+const openBtn = document.querySelector('.open');
+const closeBtn = document.querySelector('.close');
+const menu = document.querySelector('.menu');
+const overlay = document.querySelector('.menu-overlay');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('hidden');
+  openBtn.classList.toggle('hidden');
+  closeBtn.classList.toggle('hidden');
+});
+
+overlay.addEventListener('click', () => {
+  menu.classList.add('hidden');
+  openBtn.classList.remove('hidden');
+  closeBtn.classList.add('hidden');
+});
