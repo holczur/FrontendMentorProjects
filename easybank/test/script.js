78e036d7376ef6443da7c81b8cb419acf75b37a2
holczur <holcz.norbert@gmail.com>
2021-06-14 19:58:40 +0200

    header section is in progress

--- easybank/test/script.js
+++
@@ -1,6 +0,0 @@
-const openNav = document.querySelector('.btn-mobile');
-const nav = document.querySelector('nav ul');
-
-openNav.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
