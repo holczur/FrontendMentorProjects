f635ac3a796a3544c688294a6919bfa58aff4891
holczur <holcz.norbert@gmail.com>
2021-06-14 07:07:57 +0200

    header section is under build

--- easybank/final/script.js
+++ easybank/final/script.js
@@ -10,6 +10,7 @@ const closeMenu = function () {
   navOverlay.classList.remove('active');
   closeBtn.classList.add('hidden');
   openBtn.classList.remove('hidden');
+  navOverlay.classList.add('hidden');
 };
 
 openBtn.addEventListener('click', () => {
@@ -17,6 +18,7 @@ openBtn.addEventListener('click', () => {
   navOverlay.classList.add('active');
   closeBtn.classList.remove('hidden');
   openBtn.classList.add('hidden');
+  navOverlay.classList.remove('hidden');
 });
 
 closeBtn.addEventListener('click', closeMenu);
