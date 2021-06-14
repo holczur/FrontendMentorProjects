30a45c57892af0e5aa8ea79384bd1a6d456a79ff
holczur <holcz.norbert@gmail.com>
2021-06-14 17:17:49 +0200

    HTML structure is ready

---
+++ easybank/mobile-first/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const openBtn = document.querySelector('.btn-open');
+const closeBtn = document.querySelector('.btn-close');
+const navMenu = document.querySelector('.nav-menu');
+const navOverlay = document.querySelector('.overlay');
+
+const closeMenu = function () {
+  navMenu.classList.remove('active');
+  navOverlay.classList.remove('active');
+  closeBtn.classList.add('hidden');
+  openBtn.classList.remove('hidden');
+  navOverlay.classList.add('hidden');
+};
+
+openBtn.addEventListener('click', () => {
+  navMenu.classList.add('active');
+  navOverlay.classList.add('active');
+  closeBtn.classList.remove('hidden');
+  openBtn.classList.add('hidden');
+  navOverlay.classList.remove('hidden');
+});
+
+closeBtn.addEventListener('click', closeMenu);
+navOverlay.addEventListener('click', closeMenu);
